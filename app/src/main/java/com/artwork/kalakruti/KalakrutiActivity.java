cdf85374a3aa04af538ee9907015ff7002a9d6b1
Dilip Kumar Rai <dilip@adobe.com>
2015-12-24 13:55:16 +0530

    Initial project checkin

---
+++ app/src/main/java/com/artwork/kalakruti/KalakrutiActivity.java
@@ -0,0 +1,13 @@
+package com.artwork.kalakruti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KalakrutiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalakruti);
+    }
+}
